095fea53b88bd769c4d9a8e7b9c2af47977852e4
Sina Sadrzadeh <sadrzadeh.sinaa@gmail.com>
2020-12-27 19:30:29 +0330

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+import { ThemeProvider } from '@material-ui/styles';
+
+import {
+  CssBaseline,
+  createMuiTheme,
+} from '@material-ui/core';
+
+const theme = createMuiTheme({
+  palette: {
+    type: 'dark',
+  },
+});
+
+ReactDOM.render(
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
